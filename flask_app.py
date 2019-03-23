631074f4a9e0d493c48c01362dc5b3e88131d083
Lukebray <lukebray@hotmail.co.uk>
2019-03-23 16:35:30 +0000

    testconnect

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,7 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ app = Flask(__name__)
 def hello_world():
     connection = mysql.connector.connect(
         user='davidplatt', password='tinwhistle',
-        host='127.0.0.1', port=tunnel.local_bind_port,
+        host='127.0.0.1',
         database='davidplatt$WeatherData',       
     )
     if connection.is_connected():
