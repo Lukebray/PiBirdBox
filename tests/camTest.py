97382226184a8d8f578c92b4bbd964171832d46c
Lukebray <lukebray@hotmail.co.uk>
2019-04-29 16:36:54 +0100

    tests working

--- tests/camTest.py
+++ tests/camTest.py
@@ -2,8 +2,7 @@ from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
-
 camera.start_preview()
-sleep(10)
+sleep(5)
 camera.capture('/home/pi/Documents/ProjectFiles/PiBirdBox/tests/testimage.jpg')
 camera.stop_preview()
