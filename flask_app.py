e01d892145026cc22147adcf1d8b6873aec35bd3
Lukebray <lukebray@hotmail.co.uk>
2019-05-02 12:52:52 +0000

    Times work. Still have hover bug

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 from datetime import datetime, timedelta
+import pandas as pd
 import mysql.connector
 import json
 
@@ -24,39 +25,114 @@ def get_data(days):
     rows = None
 
     value = datetime.now() - timedelta(days)
-
-    c.execute("SELECT temp, humidity, rain, datetime FROM data WHERE datetime >= '%s' ORDER BY datetime DESC;" % (value))
-
-    rows = c.fetchall()
+    print(value)
 
     tempData = []
     humData = []
     rainData = []
     labels = []
-    for r in rows:
-        data = list(r)
-        temp = data[0]
-        tempData.insert(0, temp)
 
-        hum = data[1]
-        humData.insert(0, hum)
+    c.execute("SELECT temp, humidity, rain, datetime FROM data ORDER BY datetime DESC LIMIT 1;")
+    currentRow = c.fetchall()
+    currentData = list(currentRow)
 
-        rain = data[2]
-        rainData.insert(0, rain)
+    currentTemp = currentData[0][0]
+    currentHum = currentData[0][1]
+    currentRain = currentData[0][2]
 
-        datetimeval = str(data[3])
-        labels.insert(0, datetimeval)
+    if days == 7 or days == 30:
+        c.execute("SELECT temp, humidity, rain, datetime FROM data WHERE datetime >= %s ORDER BY datetime DESC;", (value,))
 
-    latestRain = rainData[-1]
-    isRaining = True
+        rows = c.fetchall()
 
-    if (latestRain == 1):
+        df = pd.DataFrame(rows, columns=['temp', 'hum', 'rain', 'date']).set_index('date')
+        df_day = df.resample('d').mean()
+        print('DF_DAY IS BELOW THIS')
+        print(df_day)
+
+        for index, row in df_day.iterrows():
+
+            tempData.insert(0, row['temp'])
+            humData.insert(0, row['hum'])
+            rainData.insert(0, row['rain'])
+            labels.insert(0, str(index.date()))
+
+        tempData.reverse()
+        humData.reverse()
+        rainData.reverse()
+        labels.reverse()
+
+        latestRain = rainData[-1]
         isRaining = True
+
+        if (latestRain == 1):
+            isRaining = True
+        else:
+            isRaining = False
+
+        return jsonify({'payload':json.dumps({'tempData':tempData, 'humData':humData, 'isRaining':isRaining, 'labels':labels, 'currentTemp':currentTemp, 'currentHum':currentHum, 'currentRain':currentRain})})
+
+    elif days == 1:
+        c.execute("SELECT temp, humidity, rain, datetime FROM data WHERE datetime >= %s ORDER BY datetime DESC;", (value,))
+
+        rows = c.fetchall()
+
+        df = pd.DataFrame(rows, columns=['temp', 'hum', 'rain', 'date']).set_index('date')
+        df_hour = df.resample('H').mean()
+        print('DF_HOUR IS BELOW THIS')
+        print(df_hour)
+
+        for index, row in df_hour.iterrows():
+
+            tempData.insert(0, row['temp'])
+            humData.insert(0, row['hum'])
+            rainData.insert(0, row['rain'])
+            labels.insert(0, str(index.time()))
+
+        tempData.reverse()
+        humData.reverse()
+        rainData.reverse()
+        labels.reverse()
+
+        latestRain = rainData[-1]
+        isRaining = True
+
+        if (latestRain == 1):
+            isRaining = True
+        else:
+            isRaining = False
+
+        return jsonify({'payload':json.dumps({'tempData':tempData, 'humData':humData, 'isRaining':isRaining, 'labels':labels, 'currentTemp':currentTemp, 'currentHum':currentHum, 'currentRain':currentRain})})
+
     else:
-        isRaining = False
+        c.execute("SELECT temp, humidity, rain, datetime FROM data ORDER BY datetime DESC LIMIT 25;")
+        currentRows = c.fetchall()
 
-    return jsonify({'payload':json.dumps({'tempData':tempData, 'humData':humData, 'isRaining':isRaining, 'labels':labels})})
+        for r in currentRows:
 
+            data = list(r)
+
+            temp = data[0]
+            tempData.insert(0, temp)
+
+            hum = data[1]
+            humData.insert(0, hum)
+
+            rain = data[2]
+            rainData.insert(0, rain)
+
+            datetimeval = str(data[3])
+            labels.insert(0, datetimeval)
+
+        latestRain = rainData[-1]
+        isRaining = True
+
+        if (latestRain == 1):
+            isRaining = True
+        else:
+            isRaining = False
+
+            return jsonify({'payload':json.dumps({'tempData':tempData, 'humData':humData, 'isRaining':isRaining, 'labels':labels, 'currentTemp':currentTemp, 'currentHum':currentHum, 'currentRain':currentRain})})
 
 if __name__ == "__main__":
     app.run()
